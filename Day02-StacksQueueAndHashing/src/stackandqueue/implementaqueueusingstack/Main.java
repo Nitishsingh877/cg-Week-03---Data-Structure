415a2a503b10aa6fa72e9a369dfb21817694e205
Nitish singh <nitishkumarsingh877@gmail.com>
2025-01-30 12:25:19 +0530

    added sort of stack using recursion

--- Day02-StacksQueueAndHashing/src/stackandqueue/implementaqueueusingstack/Main.java
+++ Day02-StacksQueueAndHashing/src/stackandqueue/implementaqueueusingstack/Main.java
@@ -1,37 +1,7 @@
 package stackandqueue.implementaqueueusingstack;
 
-import java.util.Stack;
-
 public class Main {
+    public static void main(String[] args) {
 
-    private static void sortedInsert(Stack<Integer> stack , int element){
-        if(stack.isEmpty() || stack.peek() <= element){
-            stack.push(element);
-        }else {
-            int x = stack.pop();
-            sortedInsert(stack, element);
-            stack.push(x);
-        }
     }
-
-    public static void sortStack(Stack<Integer> stack){
-        if(!stack.isEmpty()){
-            int x= stack.pop();
-            sortStack(stack);
-            sortedInsert(stack,x);
-        }
-    }
-     public static void main(String[] args) {
-         Stack<Integer> stack = new Stack<>();
-
-         stack.push(5);
-         stack.push(1);
-         stack.push(0);
-         stack.push(2);
-
-         System.out.println("original stack " + stack);
-         sortStack(stack);
-         System.out.println("sorted stack " + stack);
-
-     }
 }
